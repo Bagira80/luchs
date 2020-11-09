5e7d8a622c24f65bb545c0b8a3533b0501ec101b
Deniz Bahadir <dbahadir@benocs.com>
2020-11-09 23:21:36 +0100

    Also find/store version of associated Debian package of/in target
    Signed-off-by: Deniz Bahadir <dbahadir@benocs.com>

--- common/Modules/Helpers/find_associated_debian_package.sh
+++ common/Modules/Helpers/find_associated_debian_package.sh
@@ -9,8 +9,21 @@ shift
 REMAINING_ARGS=$@
 
 # Determine Debian package for given search-file.
-package_name=$(dpkg -S $SEARCH_FILE 2>&1)
+package_name=$(dpkg-query -S $SEARCH_FILE 2>&1)
 package_name=$(echo "$package_name" | tail -n 1)
 package_name=$(echo $package_name | sed 's/:.*$//')
-# Output found package and remaining arguments (if any).
-echo $package_name $REMAINING_ARGS
+
+# Output remaining arguments if no package found and return.
+if [ -z "$package_name" ]; then
+    echo "\t\t$REMAINING_ARGS"
+    return
+fi
+
+# Determine version of Debian package.
+package_version=$(dpkg-query --showformat='${Version}' --show $package_name)
+# Output found package/version and remaining arguments (if any).
+if [ -z "$package_version" ]; then
+    echo "$package_name\t$REMAINING_ARGS"
+else
+    echo "$package_name\t$package_version\t$REMAINING_ARGS"
+fi
