9f09acd1f37a4ed3024f36e0c99f4afc61b8a821
Deniz Bahadir <deniz@code.bahadir.email>
2022-01-14 15:42:23 +0100

    Load company-specific information as "pre" action
    The file that contains company-specific information will be loaded once
    from the first "pre" actions script.
    By default, it needs to be provided by the project that will be built.
    In case that none will be provided such a file will be generated and
    that one will then be loaded instead.
    
    Signed-off-by: Deniz Bahadir <deniz@code.bahadir.email>

--- framework/Modules/common-project-pre-actions.cmake
+++ framework/Modules/common-project-pre-actions.cmake
@@ -5,11 +5,45 @@
 #
 # @details Includes the file with project-specific information for the currently being processed
 #          CMakeLists.txt file. This information contains the project's description, version and
-#          other useful information.
+#          other useful information.  
+#          Additionally, tries to load a file with company-specific information for the entire
+#          CMake build if it had not been loaded already. If none can be found a new one will be
+#          generated and loaded instead (if a suitable template exists).
 # @note The file with the project-specific information needs to be named `project-info.cmake` and
 #       must be located in a subdirectory `cmake` of the currently being processed CMakeLists.txt
 #       file.
+# @note The file with the company-specific information needs to be named `company-info.cmake` and
+#       must be located in a subdirectory `cmake` of the currently being processed CMakeLists.txt
+#       file, if it exists at all. A suitable template must be named `company-info.cmake.in` and
+#       will be searched for in this CMake framework's `templates` directory.
 #
+
+
+# Load the company-specific information (if not already).
+# Note: If no such information file can be loaded, generate one and load that instead.
+if (NOT COMPANY_INFO_LOADED)
+    include( "${CMAKE_CURRENT_SOURCE_DIR}/luchs/company-info.cmake" OPTIONAL
+             RESULT_VARIABLE COMPANY_INFO_LOADED )
+    if (NOT COMPANY_INFO_LOADED)
+        if (EXISTS "${LUCHS_TEMPLATES_DIR}/company-info.cmake.in")
+            configure_file( "${LUCHS_TEMPLATES_DIR}/company-info.cmake.in"
+                            "${CMAKE_CURRENT_BINARY_DIR}/luchs/company-info.cmake"
+                            COPYONLY )
+            include( "${CMAKE_CURRENT_BINARY_DIR}/luchs/company-info.cmake"
+                     RESULT_VARIABLE COMPANY_INFO_LOADED )
+        else()
+            message( DEBUG "Can neither load nor generate a 'company-info.cmake' file for directory: ${CMAKE_CURRENT_SOURCE_DIR}" )
+            set( COMPANY_ID                   "Unknown" )
+            set( COMPANY_NAME                 "Unknown" )
+            set( COMPANY_FOUNDING_YEAR        "?" )
+            set( COMPANY_WEBSITE              "" )
+            set( COMPANY_EMAIL                "unknown@unknown" )
+            set( COMPANY_SUPPORT_EMAIL        "unknown@unknown" )
+            set( COMPANY_SUPPORT_NAME         "Support Team" )
+            set( COMPANY_GROUP_PACKAGE_NAME   "unknown" )
+        endif()
+    endif()
+endif()
 
 
 # Load the project-specific information.
