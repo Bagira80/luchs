b0980d7e139c159f3038d2c01ef063d92b5b0d31
Deniz Bahadir <deniz@code.bahadir.email>
2022-02-24 18:07:55 +0100

    Add tool.-settings function `enforce_minimal_supported_windows_version`
    This is a function which instructs the linker to enforce a minimal
    Windows version that shall be supported. (Of course, all this is only
    done if compiling for the Windows platform!)
    
    Signed-off-by: Deniz Bahadir <deniz@code.bahadir.email>

--- framework/Modules/ToolchainSettings/windows_specific_settings.cmake
+++ framework/Modules/ToolchainSettings/windows_specific_settings.cmake
@@ -2,6 +2,7 @@
 # @file
 # @details This file defines functions which make Windows specific settings to compiler/linker.
 #          * A function that generates the `targetver.h` header and includes it by default.
+#          * A function that enforces the minimal supported windows version.
 #
 
 
@@ -33,5 +34,25 @@ function( generate_and_auto_include_targetver_header )
                              "$<$<AND:$<COMPILE_LANG_AND_ID:CXX,Clang>,$<STREQUAL:${CMAKE_CXX_COMPILER_FRONTEND_VARIANT},GNU>>:-imacros${CMAKE_BINARY_DIR}/src/targetver.h>"
                              "$<$<AND:$<COMPILE_LANG_AND_ID:C,Clang>,$<STREQUAL:${CMAKE_C_COMPILER_FRONTEND_VARIANT},GNU>>:-imacros${CMAKE_BINARY_DIR}/src/targetver.h>"
         )
+    endif()
+endfunction()
+
+
+##
+# @name enforce_minimal_supported_windows_version()
+# @brief Enforce mimimal supported Windows version for executables/DLLs.
+# @note This only has any effect when compiling with MSVC or a compiler simulating MSVC.
+#
+function( enforce_minimal_supported_windows_version )
+    if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+        # Calculate required minimal Windows version appropriately.
+        if (NOT CMAKE_SYSTEM_VERSION)
+            message( FATAL_ERROR "Variable CMAKE_SYSTEM_VERSION is not set!" )
+        endif()
+        string( REGEX REPLACE "^([0-9]+)[.].*"       "\\1" win_major_version "${CMAKE_SYSTEM_VERSION}" )
+        string( REGEX REPLACE "^[0-9]+[.]([0-9]+).*" "\\1" win_minor_version "${CMAKE_SYSTEM_VERSION}" )
+        message( DEBUG "Enforcing minimal supported Windows version: ${win_major_version}.${win_minor_version}" )
+        # Enforce that minimal supported Windows version when linking.
+        add_link_options( "LINKER:SHELL:/SUBSYSTEM:$<IF:$<BOOL:$<TARGET_PROPERTY:WIN32_EXECUTABLE>>,WINDOWS,CONSOLE>,${win_major_version}.${win_minor_version}" )
     endif()
 endfunction()
