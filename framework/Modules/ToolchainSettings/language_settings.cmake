7e9d3c5370922f49a394c928550f6984167e998e
Deniz Bahadir <deniz@code.bahadir.email>
2022-03-01 15:49:48 +0100

    Add tool.-settings macro `set_minimum_required_c_standard`
    This is a macro which sets the minimal required C standard.
    
    Currently, this is C17 (without any compiler-extensions) if CMake
    already knows it, otherwise C11.
    
    Signed-off-by: Deniz Bahadir <deniz@code.bahadir.email>

--- framework/Modules/ToolchainSettings/language_settings.cmake
+++ framework/Modules/ToolchainSettings/language_settings.cmake
@@ -2,6 +2,7 @@
 # @file
 # @details This file defines functions/macros which make programming language specific settings.
 #          * A macro that sets the required minimal C++ standard to be used.
+#          * A macro that sets the required minimal C standard to be used.
 #
 
 
@@ -19,4 +20,21 @@ macro( set_minimum_required_cxx_standard )
        (CMAKE_CXX_SIMULATE_ID STREQUAL "MSVC" AND CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC"))
        add_compile_options( "/Zc:__cplusplus" )
    endif()
+endmacro()
+
+
+##
+# @name set_minimum_required_c_standard()
+# @brief Sets the minimal required C standard (and disables extensions).
+#
+macro( set_minimum_required_c_standard )
+    # Require at least C11 (and turn off non-standard compiler extensions).
+    set( CMAKE_C_STANDARD          11 )
+    set( CMAKE_C_STANDARD_REQUIRED ON )
+    set( CMAKE_C_EXTENSIONS        OFF )
+    # If supported by CMake, require C17 instead!
+    if (CMAKE_VERSION VERSION_GREATER_EQUAL 3.22 OR
+       (CMAKE_VERSION VERSION_GREATER_EQUAL 3.21 AND NOT CMAKE_C_COMPILER_ID STREQUAL "MSVC"))
+        set( CMAKE_C_STANDARD     17 )
+    endif()
 endmacro()
